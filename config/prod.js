7f9147d694fc693bb3e59cb1802d042819d78b18
Gilad Sterman <giladsterman1999@gmail.com>
2023-11-06 14:23:11 +0200

    fix config

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
 export default {
-    // dbURL: `mongodb+srv://gilad1999:${process.env.MONGOSEC}@cluster0.z8fzxeo.mongodb.net/?retryWrites=true&w=majority`,
-    dbURL: `mongodb+srv://gilad:${process.env.MONGOSEC}@cluster0.zpcdbb3.mongodb.net/?retryWrites=true&w=majority`,
+    dbURL: process.env.DB_URL,
     dbName: 'tester_db',
 }
